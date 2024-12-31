bfe1305f2636feca1acc8e3277e031b6f579c41a
undefinedDen <tunevdenis123@gmail.com>
2024-12-31 08:43:58 +0200

    initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Main {
+    //multyplayPair имеет значение 1 потому что если оно будет 0 то  при умножении ответ всегда ноль
+    //TODO: добавить проверку на пустой масив
+    static int multyplayPair = 1;
+    static int multyplayNot = 1;
+    static int sumPair;
+    static int sumNotPair;
+    final static int ROWANDCOLUMN = 4;
+    public static void main(String[] args) {
+    }
+}
